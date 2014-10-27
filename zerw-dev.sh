35bfbafb717d8381de768294595a03e21d3b6a99
Ivan Avila <ivelaval@gmail.com>
2014-10-27 01:27:25 -0500

    modules were updated and made ​​improvements in functionality. module form, badge, label, image was completed, addon

---
+++ zerw-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sass --watch _zerw.scss:template/style.css --style expanded
+
+exit 0
+
---
+++ zerw-dist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sass --watch _zerw.scss:template/style.min.css --style compressed
+
+exit 0
+
