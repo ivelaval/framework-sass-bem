56bdaf63589c70aec7451577d16530a76ba097af
Ivan Avila <ivelaval@gmail.com>
2014-11-09 20:03:24 -0500

    update modules

--- zerw-dev.sh
+++ zerw-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sass --watch _zerw.scss:template/style.css --style expanded --stop-on-error --trace
+#sass --watch _zerw.scss:template/style.css --style expanded --stop-on-error --trace
+sass --watch _zerw.scss:template/style.css --style expanded
 
 exit 0
 
